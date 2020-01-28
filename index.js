3914b9d983d3951f39f2e3251758a8be7bcc02bb
BuildTools <78happydragon78@gmail.com>
2020-01-27 23:05:04 -0500

    First Commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+function createRoom() {
+    if(document.getElementsByClassName("text_field").value == undefined) {
+        alert("You must type in a name first!")
+    }
+}
+
+function joinRoom() {
+    if(document.getElementsByClassName("text_field").value == undefined) {
+        alert("You must type in a name first!")
+    }
+}
