49f62650a645d3dc47ca692dce831d80c67cf4c1
BuildTools <78happydragon78@gmail.com>
2020-01-28 22:50:56 -0500

    Started using socket

---
+++ Public/button_handler.js
@@ -0,0 +1,29 @@
+function createRoom() {
+    let name_field = document.querySelector('.name_field');
+
+    if(name_field.value == "" || name_field.value.replace(/\s/g, '').length == "") {
+        name_field.color = 0xff0000;
+        name_field.placeholder = "You must type in your name!";
+    } else {
+        window.location.href = "create_room.html"
+    }
+    
+}
+
+function joinRoom() {
+    let name_field = document.querySelector('.name_field');
+    
+    if(name_field.value == "" || name_field.value.replace(/\s/g, '').length == "") {
+        name_field.color = 0xff0000;
+        name_field.placeholder = "You must type in your name!";
+    } else {
+        window.location.href = "create_room.html"
+    }
+}
+
+function hasWhiteSpaces(string) {
+    if (string.length == string.replace(" ", "").length) {
+        return false
+    }
+    return true
+}
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function createRoom() {
-    if(document.getElementsByClassName("text_field").value == undefined) {
-        alert("You must type in a name first!")
-    }
-}
+var express = require('express');
+var socket = require('socket.io');
+var fs = require('fs');
+const rooms = JSON.parse(fs.readFileSync('Storage/rooms.json', 'utf8'));
 
-function joinRoom() {
-    if(document.getElementsByClassName("text_field").value == undefined) {
-        alert("You must type in a name first!")
+var app = express();
+
+var server = app.listen(4000, function(){
+    console.log('Listening to port 4000');
+});
+app.use(express.static('Public'));
+
+var io = socket(server);
+
+io.on('connection', socket => {
+    var address = socket.handshake.address;
+    console.log('New connection from ' + socket.id);
+    //console.log(Object.keys(io.sockets.sockets));
+
+/*    
+    if(!rooms["hey"]) {
+        rooms["hey"] = {}
     }
-}
+    if(!rooms["hey"]["yo"]) {
+        rooms["hey"]["yo"] = {}
+    }
+
+    fs.writeFile('Storage/rooms.json', JSON.stringify(rooms, null, 4), (err) => {
+        if(err) console.error(err);
+    });*/
+});
+
